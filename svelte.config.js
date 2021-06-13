f4b04cdc944d696179f31c2e27c2f6112fe63842
D34dMan <shibin@factorial.io>
2021-06-13 18:25:03 +0200

    Update netlify adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,17 +1,8 @@
-import preprocess from 'svelte-preprocess';
-import netlify from '@sveltejs/adapter-netlify';
+import adapter from '@sveltejs/adapter-netlify';
 
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
-	// Consult https://github.com/sveltejs/svelte-preprocess
-	// for more information about preprocessors
-	preprocess: preprocess(),
-
+export default {
 	kit: {
-		adapter: netlify(),
-		// hydrate the <div id="svelte"> element in src/app.html
+		adapter: adapter(), // currently the adapter does not take any options
 		target: '#svelte'
 	}
 };
-
-export default config;
