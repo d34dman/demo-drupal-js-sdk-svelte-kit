eb7159e3f91153962f28fc9387768cbbf2d528c2
D34dMan <shibin@factorial.io>
2022-03-25 22:07:54 +0100

    fix: svelte.config.js configuration where "target" is not required anymore

--- svelte.config.js
+++ svelte.config.js
@@ -2,7 +2,6 @@ import adapter from '@sveltejs/adapter-netlify';
 
 export default {
 	kit: {
-		adapter: adapter(), // currently the adapter does not take any options
-		target: '#svelte'
+		adapter: adapter()
 	}
 };
