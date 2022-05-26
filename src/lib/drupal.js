3b0a8b9c2053996bb5788894bea66c8ec9c028fb
D34dMan <shibin@factorial.io>
2022-05-26 10:57:08 +0200

    fix errors in drupal-js-sdk after upgrade to 0.3.10

--- src/lib/drupal.js
+++ src/lib/drupal.js
@@ -1,10 +1,15 @@
-import { Drupal, DrupalAuth, DrupalMenu } from 'drupal-js-sdk';
+import { Drupal } from 'drupal-js-sdk';
+import { DrupalAuth } from '@drupal-js-sdk/auth';
+import { DrupalMenu } from '@drupal-js-sdk/menu';
+import { StorageInMemory } from '@drupal-js-sdk/storage';
 
 const config = {
 	baseURL: import.meta.env.VITE_DRUPAL_BASE_URL
 };
 
-export const drupal = new Drupal(config);
-export const auth = new DrupalAuth(drupal);
-export const drupalMenu = new DrupalMenu(drupal);
-export default drupal;
+export const sdk = new Drupal(config);
+
+sdk.setSessionService(new StorageInMemory());
+export const auth = new DrupalAuth(sdk);
+export const menu = new DrupalMenu(sdk);
+export default sdk
