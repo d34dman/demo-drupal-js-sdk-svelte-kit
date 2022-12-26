a47173d899dd7a5e54c43898e693662ff68d8b9c
D34dMan <shibin@factorial.io>
2022-12-26 04:21:16 +0100

    Revert "fix errors in drupal-js-sdk after upgrade to 0.3.10"
    This reverts commit 3b0a8b9c2053996bb5788894bea66c8ec9c028fb.

--- src/lib/drupal.js
+++ src/lib/drupal.js
@@ -1,15 +1,10 @@
-import { Drupal } from 'drupal-js-sdk';
-import { DrupalAuth } from '@drupal-js-sdk/auth';
-import { DrupalMenu } from '@drupal-js-sdk/menu';
-import { StorageInMemory } from '@drupal-js-sdk/storage';
+import { Drupal, DrupalAuth, DrupalMenu } from 'drupal-js-sdk';
 
 const config = {
 	baseURL: import.meta.env.VITE_DRUPAL_BASE_URL
 };
 
-export const sdk = new Drupal(config);
-
-sdk.setSessionService(new StorageInMemory());
-export const auth = new DrupalAuth(sdk);
-export const menu = new DrupalMenu(sdk);
-export default sdk
+export const drupal = new Drupal(config);
+export const auth = new DrupalAuth(drupal);
+export const drupalMenu = new DrupalMenu(drupal);
+export default drupal;
