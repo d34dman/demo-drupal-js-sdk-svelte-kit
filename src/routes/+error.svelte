26f9ff12896a5e047b8569d578f2ee62defe79b9
D34dMan <shibin@factorial.io>
2022-12-26 04:35:59 +0100

    Add an error.svelte

---
+++ src/routes/+error.svelte
@@ -0,0 +1,10 @@
+<div class="grid grid-cols-4 gap-4">
+  <div class="col-start-2 col-span-2 m-10 alert alert-error shadow-lg">
+    <div>
+        <svg xmlns="http://www.w3.org/2000/svg" class="stroke-current flex-shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
+        <span>Error! Demo is down since the app can't connect to a Drupal backend. Reason, I stopped paying my server fees doh!</span>
+    </div>
+    <div>   </div>
+    </div>
+</div>
+
